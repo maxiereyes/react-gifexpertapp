ec4ff82434eff5850f1d2610c693a100d780a3ad
Maximiliano Reyes <maximilianoreyes@iMac-de-Maximiliano.local>
2020-06-22 20:20:55 -0300

    Modify http by https giphy

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 const apiKey = "EkolPnH1wXoGFnapSNy3sDsymGErLCYR";
-const urlBase = "http://api.giphy.com/v1/gifs/search";
+const urlBase = "https://api.giphy.com/v1/gifs/search";
 
 export const getGifs = async (category) => {
   const urlSearch = `${urlBase}?q=${encodeURI(category)}&api_key=${apiKey}`;
